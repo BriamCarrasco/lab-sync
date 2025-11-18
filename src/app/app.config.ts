b83ae0a99a0d430614fac40db22e3a10c092ce4b
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-18 18:39:02 -0300

    Se agrega ngx-toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
@@ -7,6 +13,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      timeOut: 3000,
+    }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('lab-sync');
+
+  constructor(private readonly toastr: ToastrService) {}
+
+  showToast() {
+    this.toastr.success('Operación exitosa', 'OK');
+  }
 }
