35eab2c1147ad38c6c2e4b795313b569bf97459f
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-19 17:58:18 -0300

    Se crean archivos para definir model y service para aplicacion

---
+++ src/app/model/Laboratory.ts
@@ -0,0 +1,9 @@
+export interface Laboratory {
+  id: number;
+  name: string;
+  address: string;
+  phone: string;
+  email: string;
+  website?: string;
+  specialty: string;
+}
---
+++ src/app/model/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  name: string;
+  lastName: string;
+  secondLastName: string;
+  userName: string;
+  rut: string;
+  email: string;
+  password: string;
+  role: string;
+}
