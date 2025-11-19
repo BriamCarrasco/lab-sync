643156d78be5f4348bca7745f76ab76d80aeca24
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-18 22:13:49 -0300

    Se crea footer para aplicacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Topbar } from "./components/topbar/topbar";
+import { Landing } from "./pages/landing/landing";
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Topbar],
+  imports: [RouterOutlet, Topbar, Landing, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
