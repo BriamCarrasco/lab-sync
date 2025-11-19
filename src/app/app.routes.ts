8161441da732022a92b27e8d9b77c8c08864e8bf
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-19 18:36:39 -0300

    Se mueven estilos a archivo style.css global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Home } from './pages/home/home';
+import { Profile } from "./pages/profile/profile";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: Landing },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'home', component: Home },
+  { path: 'profile', component: Profile },
 ];
--- src/app/components/topbar/topbar.ts
+++ src/app/components/topbar/topbar.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './topbar.html',
   styleUrl: './topbar.css',
 })
 export class Topbar {
+  constructor(public router: Router) {}
 
+  isLogin() {
+    return this.router.url === '/login';
+  }
+  isRegister() {
+    return this.router.url === '/register';
+  }
+  isHome() {
+    return this.router.url === '/home' || this.router.url === '/landing';
+  }
+  isProfile() {
+    return this.router.url === '/profile';
+  }
 }
