59daa9aa4f5acedcd5db777e8e616f742d5bb281
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-19 16:20:07 -0300

    Se crea vista para login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
+  isSubmitted = false;
 
+  constructor(private readonly fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) return;
+
+    console.log(this.loginForm.value);
+  }
 }
